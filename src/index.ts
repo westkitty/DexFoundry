3447ddb3227429a0d11ba0c7f11e9e5313493d54
westkitty <andrew.dolby@westcat.ca>
2026-09-17 18:36:38 -0400

    Export accessibility regression watch adapter

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,4 @@ export * from "./orchestration/workflowResult.js";
 export * from "./orchestration/resultIngestor.js";
 export * from "./orchestration/dispatcher.js";
 export * from "./offers/contracts.js";
+export * from "./offers/accessibilityRegressionWatch.js";
