517a5792d304c21ee3563ae7e1dbc61cbabf9449
westkitty <andrew.dolby@westcat.ca>
2026-09-17 19:53:19 -0400

    Export outbound policy model

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,4 @@ export * from "./offers/contracts.js";
 export * from "./offers/accessibilityRegressionWatch.js";
 export * from "./offers/pa11yScanner.js";
 export * from "./offers/accessibilityRegressionDelta.js";
+export * from "./outbound/policy.js";
